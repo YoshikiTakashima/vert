d0fa0725fb1cb897f6321571ec07438d5e71535e
Yoshiki Takashima <ytakashima@andrew.cmu.edu>
2023-12-22 10:19:49 -0800

    Add stub file to parse_int. Output is still incorrect.

--- ptr_4_manual_eq_verus/src/lib.rs
+++ ptr_4_manual_eq_verus/src/lib.rs
@@ -2,3 +2,4 @@
 
 mod avl;
 mod libtree;
+mod parseint;
---
+++ ptr_4_manual_eq_verus/src/parseint.rs
@@ -0,0 +1,10 @@
+//! parseint.rs --- Equivalence checking for `brotli_parseint`.
+//!
+//! The LLM output is not correct on this one. Wee should wait until
+//! this is corrected.
+
+use vstd::prelude::*;
+
+verus! {
+
+} // verus!
