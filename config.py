3303599152c8f5ae7fc694681ea3b259b858d512
Ubuntu <ubuntu@ip-172-31-44-0.ec2.internal>
2025-04-14 14:51:20 +0000

    set config paths for docker container

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
-here = os.getcwd()
-wasi_path=f"{here}/wasi-sdk-12.0"
-rWasm_path=f"{here}/rWasm"
+#here = os.getcwd()
+#wasi_path=f"{here}/wasi-sdk-12.0"
+#rWasm_path=f"{here}/rWasm"
+wasi_path=f"/wasi-sdk-12.0"
+rWasm_path=f"/rWasm"
 
 all_timeout=200
 bolero_timeout=2
