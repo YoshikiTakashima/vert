d22c416d26e5f7ba7fe222e83b354aca989c2dde
EC2 Default User <ec2-user@ip-172-31-44-9.ec2.internal>
2025-04-14 14:19:11 +0000

    reduce number of compilation retries

--- torust.py
+++ torust.py
@@ -296,7 +296,7 @@ def main():
     use_claude = 1  # Generate LLM transpilation
     bolero = 1  # Bolero verification
     bounded_kani = 1  # Bounded Kani verification
-    number_tries = 20  # Number of tries for LLM
+    number_tries = 5  # Number of tries for LLM
     ####################################################################################################
 
     subdir = args.benchmark_dir
